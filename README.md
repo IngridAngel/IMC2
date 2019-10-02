e8bdc6881d3b78acb3be9f163b929f338a41b650
Ingird Ocaña Angel <16470300@itcampeche.edu.mx>
2019-10-01 19:38:01 -0500

    Version inical dee proyecto interfaz de usuarip y n incluye manejo de eventos

---
+++ README.md
@@ -0,0 +1,1 @@
+# Apliccion que calcula el indice de Masa Corporal
---
+++ app/src/main/java/com/ingridangel/android/imc2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ingridangel.android.imc2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
